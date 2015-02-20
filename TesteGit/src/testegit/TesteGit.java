0bc8947e2f08befd570abb351ce8762f37325128
Maumal <Maumal@gmail.com>
2015-02-20 12:19:20 -0200

    6 commit

--- TesteGit/src/testegit/TesteGit.java
+++ TesteGit/src/testegit/TesteGit.java
@@ -1,37 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package testegit;
 
-/**
- *
- * @author Maurício
- */
 public class TesteGit {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        
-        
+  
+    public static void main(String[] args) {       
         System.out.println("1 commit");
         System.out.println("2 commit");
         System.out.println("3 commit");
         System.out.println("4 commit");
         System.out.println("5 commit");
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        System.out.println("6 commit");
+        System.out.println("6 commit");
+        System.out.println("6 commit");               
     }
     
 }
